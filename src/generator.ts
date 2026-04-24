b584c9741f6abc56acffa86c445aa177463f1327
SWvs <59214670+sammy0101@users.noreply.github.com>
2026-04-24 19:32:05 +0800

    Initialize group.proxies as an empty array

--- src/generator.ts
+++ src/generator.ts
@@ -119,9 +119,9 @@ export async function toClashWithTemplate(nodes: ProxyNode[]) {
 
   if (Array.isArray(config['proxy-groups'])) {
     config['proxy-groups'].forEach((group: any) => {
-      if (!Array.isArray(group.proxies)) group.proxies =
-       const currentProxies = new Set(group.proxies);
-       proxyNames.forEach((name: string) => {
+      if (!Array.isArray(group.proxies)) group.proxies = [];
+      const currentProxies = new Set(group.proxies);
+      proxyNames.forEach((name: string) => {
           if (!currentProxies.has(name)) {
               group.proxies.push(name);
           }
